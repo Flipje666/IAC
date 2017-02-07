694ff3e1a99741aac2c3f3c41090bea60c83041c
Flipje <philip.vanexel@student.hu.nl>
2017-02-07 10:44:47 +0100

    sysout in main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 /**
- * Created by Philip on 2/7/2017.
+ * Gemaakt door Philip op 07/02/2017.
  */
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("TEST");
     }
 }
